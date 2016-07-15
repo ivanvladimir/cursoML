566d784a04881391faa66ad055301fba6d2e2edf
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2016-07-14 23:24:42 -0500

    Session 5 for text

---
+++ sesion5_text/create_segments.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Ivan Vladimir Meza-Ruiz/ ivanvladimir at turing.iimas.unam.mx
+# 2016/IIMAS/UNAM
+# ----------------------------------------------------------------------
+
+# Cargando librerias
+from __future__ import division, print_function  # Python 2 users only
+import argparse
+import nltk, re, pprint
+from nltk import word_tokenize
+
+
+if __name__ == "__main__":
+    # Command line options
+    p = argparse.ArgumentParser("Documentos")
+    p.add_argument('LISTA',help="Archivo con lista",nargs='+')
+    p.add_argument("-t", "--train",default=0.7,
+            action="store", dest="train",
+            help="Percentage of document for training [0.7]")
+    p.add_argument("-s", "--size",default=500,
+            action="store", dest="size",
+            help="Number of tokens in segment [500]")
+    p.add_argument("-j", "--jump",default=250,
+            action="store", dest="jump",
+            help="Skip size betweent tokens [250]")
+    p.add_argument("-v", "--verbose",
+            action="store_true", dest="verbose",
+            help="Modo verbose [Off]")
+    p.add_argument('--version', action='version', version='create_segments 0.1')
+    opts = p.parse_args()
+
+    # Prepara función de verbose  -----------------------------------------
+    if opts.verbose:
+        def verbose(*args,**kargs):
+            print(*args,**kargs)
+    else:   
+        verbose = lambda *a: None 
+
+
+    segs_train=[]
+    segs_test=[]
+    for i,filename in enumerate(opts.LISTA):
+        raw = open(filename).read().decode('utf8')
+        tokens = word_tokenize(raw)
+        # training segment
+        train = tokens[0:int(0.7*len(tokens))]
+        for j in range(0,len(train),opts.jump):
+            seg=train[j:j+opts.size]
+            segs_train.append((" ".join(seg).encode('utf8'),i))
+        # testing segment
+        test = tokens[int(0.7*len(tokens)):]
+        for j in range(0,len(test),opts.jump):
+            seg=test[j:j+opts.size]
+            segs_test.append((" ".join(seg).encode('utf8'),i))
+
+
+    examples=open("train.txt",'w')
+    for seg in segs_train:
+        print(seg[0],seg[1],file=examples)
+    examples.close()
+    examples=open("test.txt",'w')
+    for seg in segs_test:
+        print(seg[0],seg[1],file=examples)
+    examples.close()
+
---
+++ sesion5_text/train_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Ivan Vladimir Meza-Ruiz/ ivanvladimir at turing.iimas.unam.mx
+# 2016/IIMAS/UNAM
+# ----------------------------------------------------------------------
+
+# Cargando librerias
+from __future__ import division, print_function  # Python 2 users only
+import argparse
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+
+if __name__ == "__main__":
+    # Command line options
+    p = argparse.ArgumentParser("Documentos")
+    p.add_argument('TRAINIG',help="Archivos con datos de entreanmiento")
+    p.add_argument('TESTING',help="Archivo con datos de prueba")
+    p.add_argument("-v", "--verbose",
+            action="store_true", dest="verbose",
+            help="Modo verbose [Off]")
+    p.add_argument('--version', action='version', version='create_segments 0.1')
+    opts = p.parse_args()
+
+    # Prepara función de verbose  -----------------------------------------
+    if opts.verbose:
+        def verbose(*args,**kargs):
+            print(*args,**kargs)
+    else:   
+        verbose = lambda *a: None 
+
+    X_train=[]
+    Y_train=[]
+    for line in open(opts.TRAINIG):
+        line=line.strip()
+        bits=line.rsplit(" ", 1)
+        X_train.append(bits[0])
+        Y_train.append(int(bits[1]))
+
+
+    X_test=[]
+    Y_test=[]
+    for line in open(opts.TRAINIG):
+        line=line.strip()
+        bits=line.rsplit(" ", 1)
+        X_test.append(bits[0])
+        Y_test.append(int(bits[1]))
+
+    count_vect = CountVectorizer()
+    X_train    = count_vect.fit_transform(X_train)
+    X_test     = count_vect.transform(X_test)
+
+    clf = SVC()
+    clf.fit(X_train, Y_train)
+
+    Y_pred=clf.predict(X_test)
+    print(classification_report(Y_test, Y_pred))
